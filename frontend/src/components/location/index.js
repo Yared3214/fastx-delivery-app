eec16b9bd9fe6be1a94815415181ce006b011209
mark133227 <etawe1627@gmail.com>
2025-10-21 21:36:07 +0300

    checkout page updated

--- frontend/src/components/location/index.js
+++ frontend/src/components/location/index.js
@@ -7,9 +7,9 @@ function Location() {
             <h2 className='text-xl'>1. Enter Delivery Address</h2>
             <div className='mt-6 mb-10'>
                 <label className='text-white block'>Create a name for your location (required)</label>
-                <input className='md:w-1/2 w-full mt-2' />
+                <input className='md:w-1/2 w-full required mt-2' />
                 <label className='text-white block mt-4'>Describe your location (required)</label>
-                <input className='md:w-1/2 w-full mt-2' />
+                <input className='md:w-1/2 w-full required mt-2' />
             </div>
             <div>
                 <img src={map} width={100} className='w-full h-[300px] object-cover rounded-lg' />
